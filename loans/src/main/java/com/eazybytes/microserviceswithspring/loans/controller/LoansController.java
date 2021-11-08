3114f0022025d39289a5d0e5cb62d0e4ee4104d5
joma74 <dev-mails@gmx.net>
2021-11-08 23:03:25 +0100

    fix: Post mapping on loan controller is missing

--- loans/src/main/java/com/eazybytes/microserviceswithspring/loans/controller/LoansController.java
+++ loans/src/main/java/com/eazybytes/microserviceswithspring/loans/controller/LoansController.java
@@ -4,6 +4,7 @@ import com.eazybytes.microserviceswithspring.loans.model.Customer;
 import com.eazybytes.microserviceswithspring.loans.model.Loans;
 import com.eazybytes.microserviceswithspring.loans.repository.LoansRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class LoansController {
         this.loansRepository = loansRepository;
     }
 
+    @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
         List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
         if (loans != null) {
