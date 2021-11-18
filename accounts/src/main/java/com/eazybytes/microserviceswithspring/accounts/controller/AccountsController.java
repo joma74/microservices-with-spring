60aea7078d22f7ace91264b672f9e302a4ccc495
joma74 <dev-mails@gmx.net>
2021-11-19 00:17:48 +0100

    feat: Connected account application to config server

--- accounts/src/main/java/com/eazybytes/microserviceswithspring/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/microserviceswithspring/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package com.eazybytes.microserviceswithspring.accounts.controller;
 
+import com.eazybytes.microserviceswithspring.accounts.config.AccountsServiceConfig;
 import com.eazybytes.microserviceswithspring.accounts.model.Accounts;
+import com.eazybytes.microserviceswithspring.accounts.model.Config;
 import com.eazybytes.microserviceswithspring.accounts.model.Customer;
 import com.eazybytes.microserviceswithspring.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountsController {
 
     private final AccountsRepository accountsRepository;
+    private final AccountsServiceConfig accountsServiceConfig;
 
     @Autowired
-    public AccountsController(AccountsRepository accountsRepository) {
+    public AccountsController(AccountsRepository accountsRepository, AccountsServiceConfig accountsServiceConfig) {
         this.accountsRepository = accountsRepository;
+        this.accountsServiceConfig = accountsServiceConfig;
     }
 
     @PostMapping("/myAccount")
@@ -26,5 +34,18 @@ public class AccountsController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/accounts/config")
+    public String getConfigDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Config config = new Config(
+                accountsServiceConfig.getMsg(),
+                accountsServiceConfig.getBuildVersion(),
+                accountsServiceConfig.getMailDetails(),
+                accountsServiceConfig.getActiveBranches()
+        );
+        String jsonStr = ow.writeValueAsString(config);
+        return jsonStr;
     }
 }
---
+++ accounts/src/main/java/com/eazybytes/microserviceswithspring/accounts/model/Config.java
@@ -0,0 +1,24 @@
+package com.eazybytes.microserviceswithspring.accounts.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class Config {
+
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+    private List<String> activeBranches;
+
+    public Config(String msg, String buildVersion, Map<String, String> mailDetails, List<String> activeBranches) {
+        this.msg = msg;
+        this.buildVersion = buildVersion;
+        this.mailDetails = mailDetails;
+        this.activeBranches = activeBranches;
+    }
+}
